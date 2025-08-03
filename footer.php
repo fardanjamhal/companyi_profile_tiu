03c56c7da0242ccb387888aac4735a1fa6bdffef
fardanjamhal <fardanjamhal@gmail.com>
2025-08-03 12:15:54 +0800

    kedua

---
+++ footer.php
@@ -0,0 +1,77 @@
+<!-- Footer Start -->
+<div class="footer wow fadeIn" data-wow-delay="0.3s">
+    <div class="container">
+        <div class="row">
+            <!-- Office Contact -->
+            <div class="col-md-6 col-lg-3">
+                <div class="footer-contact">
+                    <h2>Office Contact</h2>
+                    <p><i class="fa fa-map-marker-alt"></i>Jl. Antang Indah Perum Bukit Antang Recidence No. 4 Kecamatan Manggala Kota Makassar</p>
+                    <p><i class="fa fa-phone-alt"></i>+62 852 9904 100</p>
+                    <p><i class="fa fa-envelope"></i>tiusaktijaya@gmail.com</p>
+                    <div class="footer-social">
+                        <a href=""><i class="fab fa-twitter"></i></a>
+                        <a href=""><i class="fab fa-facebook-f"></i></a>
+                        <a href=""><i class="fab fa-youtube"></i></a>
+                        <a href=""><i class="fab fa-instagram"></i></a>
+                        <a href=""><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+            </div>
+            <!-- Services Areas -->
+            <div class="col-md-6 col-lg-3">
+                <div class="footer-link">
+                    <h2>Services Areas</h2>
+                    <a href="">Building Construction</a>
+                    <a href="">House Renovation</a>
+                    <a href="">Architecture Design</a>
+                    <a href="">Interior Design</a>
+                    <a href="">Painting</a>
+                </div>
+            </div>
+            <!-- Useful Pages -->
+            <div class="col-md-6 col-lg-3">
+                <div class="footer-link">
+                    <h2>Useful Pages</h2>
+                    <a href="">About Us</a>
+                    <a href="">Contact Us</a>
+                    <a href="">Our Team</a>
+                    <a href="">Projects</a>
+                    <a href="">Testimonial</a>
+                </div>
+            </div>
+            <!-- Newsletter -->
+            <div class="col-md-6 col-lg-3">
+                <div class="newsletter">
+                    <h2>Newsletter</h2>
+                    <p>
+                        Kami adalah perusahaan yang bergerak di bidang jasa konstruksi dan pengembangan real estate, berkomitmen menghadirkan solusi pembangunan yang tepat, berkualitas, dan terpercaya.
+                    </p>
+                    <div class="form">
+                        <input class="form-control" placeholder="Email here">
+                        <button class="btn">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer Menu -->
+    <div class="container footer-menu">
+        <div class="f-menu">
+            <a href="">Terms of use</a>
+            <a href="">Privacy policy</a>
+            <a href="">Cookies</a>
+            <a href="">Help</a>
+            <a href="">FQAs</a>
+        </div>
+    </div>
+    <!-- Copyright -->
+    <div class="container copyright">
+        <div class="row justify-content-center">
+            <div class="col-auto text-center">
+                <p>&copy; <a href="#">www.tiusaktijaya.com</a></p>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Footer End -->
